d00a4cecbc852dc41b9ecc28f1cc7d8535c8cc50
twfinderaz <twfinderaz@gmail.com>
2023-05-18 10:39:50 +0800

    Delete test2.py

--- test2.py
+++
@@ -1,6 +0,0 @@
-# test.py
-def test_github_token():
-    token = "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"
-    print(f"This is a fake GitHub token: {token}")
-
-test_github_token()
