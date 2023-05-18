50a376ffed8b099eccccbe75ce12e7c2aa6993a8
twfinderaz <twfinderaz@gmail.com>
2023-05-18 10:35:18 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 # test.py
 def test_github_token():
-    token = "ghp_1234567890ABCDEF1234567890ABCDEF12345678"
+    token = "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"
     print(f"This is a fake GitHub token: {token}")
 
 test_github_token()
