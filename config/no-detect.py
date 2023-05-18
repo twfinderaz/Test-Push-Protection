fcf191760576bc8a9cb70da5b2c0b1e8801bec93
twfinderaz <twfinderaz@gmail.com>
2023-05-18 11:44:41 +0800

    Create no-detect.py

---
+++ config/no-detect.py
@@ -0,0 +1,6 @@
+# test.py
+def test_github_token():
+    token = "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"
+    print(f"This is a fake GitHub token: {token}")
+
+test_github_token()
