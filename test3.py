e07a1e5889992d7d67346e9906ae0b770474b142
twfinderaz <twfinderaz@gmail.com>
2023-05-19 10:39:48 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,6 +1,6 @@
 # test.py
 def test_github_token():
-    token = "password123"
+    token = "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"
     print(f"This is a fake GitHub token: {token}")
 
 test_github_token()
